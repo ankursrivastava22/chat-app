c12fe40491c9715069b0ab1d368b68254693d921
ankur <dec22ankur@gmail.com>
2020-03-30 19:49:20 +0530

    Updates

---
+++ precache-manifest.e00dc24750010bf5aeff324fddcb5778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89e39874c2f787897d67984b01c3f69",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "76cfef087429661bd555",
+    "url": "/chat-app/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "065bf5b7d448678a5155",
+    "url": "/chat-app/static/js/2.5c806cb3.chunk.js"
+  },
+  {
+    "revision": "76cfef087429661bd555",
+    "url": "/chat-app/static/js/main.70196f50.chunk.js"
+  },
+  {
+    "revision": "ecb668af3d80c049442a",
+    "url": "/chat-app/static/js/runtime~main.1d27bffe.js"
+  }
+]);
